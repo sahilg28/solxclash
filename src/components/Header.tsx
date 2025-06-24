96be17e5c14f532834f4a3887d7c3168ba602d7e
SeeGee <86075017+sahilg28@users.noreply.github.com>
2025-06-24 20:27:40 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -56,7 +56,7 @@ const Header = () => {
           <div className="flex items-center justify-between h-16">
             {/* Logo */}
             <Link to="/" className="flex items-center space-x-2 hover:opacity-80 transition-opacity duration-200">
-              <div className="w-10 h-10 rounded overflow-hidden">
+              <div className="w-10 h-10 rounded">
                 <img 
                   src="/assets/solxclash_logo.svg" 
                   alt="SolxClash" 
