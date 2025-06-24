7b4bf60324f18fb99d5fb2eb0dc436599d78e9c3
SeeGee <86075017+sahilg28@users.noreply.github.com>
2025-06-24 12:42:46 +0530

    Updated index.html

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { Menu, X, User, ChevronDown, LogOut, Loader2 } from 'lucide-react';
+import { Menu, X, Zap, User, ChevronDown, LogOut, Loader2 } from 'lucide-react';
 import { useAuthContext } from './AuthProvider';
 import AuthButtons from './AuthButtons';
 
@@ -55,11 +55,9 @@ const Header = () => {
           <div className="flex items-center justify-between h-16">
             {/* Logo */}
             <Link to="/" className="flex items-center space-x-2 hover:opacity-80 transition-opacity duration-200">
-              <img 
-                src="/solxclash_logo.png" 
-                alt="SolxClash" 
-                className="w-8 h-8"
-              />
+              <div className="w-8 h-8 bg-yellow-400 rounded-lg flex items-center justify-center">
+                <Zap className="w-5 h-5 text-black" />
+              </div>
               <span className="text-xl font-bold text-white">SolxClash</span>
             </Link>
 
@@ -118,11 +116,9 @@ const Header = () => {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-2 hover:opacity-80 transition-opacity duration-200">
-            <img 
-              src="/solxclash_logo.png" 
-              alt="SolxClash" 
-              className="w-8 h-8"
-            />
+            <div className="w-8 h-8 bg-yellow-400 rounded-lg flex items-center justify-center">
+              <Zap className="w-5 h-5 text-black" />
+            </div>
             <span className="text-xl font-bold text-white">SolxClash</span>
           </Link>
 
